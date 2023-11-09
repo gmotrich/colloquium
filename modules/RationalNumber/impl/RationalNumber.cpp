149911835a9a5c09e5ddb846c0bf2a705741f732
gmotrich <demetrashvili.dm@gmail.com>
2023-11-09 14:39:39 +0300

    adding a check for denominator == 0

--- modules/RationalNumber/impl/RationalNumber.cpp
+++ modules/RationalNumber/impl/RationalNumber.cpp
@@ -1,7 +1,11 @@
 #include "../api/RationalNumber.h"
 
 
-RationalNumber::RationalNumber(int n, int d) : numerator(n), denominator(d) {}
+RationalNumber::RationalNumber(int n, int d) : numerator(n), denominator(d) {
+    if (denominator == 0) {
+        throw std::invalid_argument("Denominator can't be zero.");
+    }
+}
 
 string RationalNumber::INT_Q_B() {
     if (denominator == 1)
